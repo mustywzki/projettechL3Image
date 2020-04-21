d1536d750ee6a287aaafe74bd6fe4beae44547f1
Bourhanoudine <bourhanoudine.mamod-houssen@etu.u-bordeaux.fr>
2020-04-21 16:16:23 +0200

    Correction Beug RotateRight

--- app/src/main/rs/rotateRight.rs
+++ app/src/main/rs/rotateRight.rs
@@ -5,5 +5,5 @@ rs_allocation gOut;
 int height, width;
 
 void RS_KERNEL rotateRight(uchar4 in, uint32_t x, uint32_t y){
-    rsSetElementAt_uchar4(gOut,in, width-(height-y-1)-1,width-x-1);
+    rsSetElementAt_uchar4(gOut,in, y,width-x-1);
 }
